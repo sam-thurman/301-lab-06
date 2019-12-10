e7f68ab86c473d8f52b3586872c4a34bbc3f629d
Sam Thurman <sam.bbmgmt@gmail.com>
2019-12-10 11:32:55 -0800

    heroku push

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
 # 301-lab-06
 
+---
+
 Number and name of feature: repo set up
 
 Estimate of time needed to complete: 45
 
 Start time: 9:50
 
-Finish time: _____
+Finish time: 10:55
 
-Actual time needed to complete: _____
+Actual time needed to complete: 1 hr 5
+
+---
+
+Number and name of feature: locations
+
+Estimate of time needed to complete: 2hr
+
+Start time: 11:10
+
+Finish time: 10:55
+
+Actual time needed to complete: 1 hr 5
+
+---
+
+Number and name of feature: repo set up
+
+Estimate of time needed to complete: 45
+
+Start time: 9:50
+
+Finish time: 10:55
+
+Actual time needed to complete: 1 hr 5
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-'use strict'
+"use strict";
 
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,23 +19,28 @@ function Location(city, geoDataResults) {
 
 // Routes (for what? i dont know)
 
-app.get('/location', (request, response) => {
-  let city = request.query.data
+app.get("/location", (request, response) => {
+  let city = request.query.data;
 
   let locationObj = searchLatToLong(city);
-
+  // const locationObj = {
+  //   "search_query": city,
+  //   "formatted_query": geoDataResults.formatted_address,
+  //   "latitude": geoDataResults.geometry.location.lat,
+  //   "longitude": geoDataResults.geometry.location.lng
+  // }
   response.send(locationObj);
 });
 
 function searchLatToLong(city) {
-  const geoData = require('./data/geo.json');
+  const geoData = require("./data/geo.json");
   const geoDataResults = geoData.results[0];
   const locationObj = new Location(city, geoDataResults);
+  return locationObj;
 }
 
-app.get('*', (request, response) => {
-  response.status(404).send('Page not found');
-})
+app.get("*", (request, response) => {
+  response.status(404).send("Page not found");
+});
 
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
