7e19c8c2a6bcf1c3c8e6dd94743ad7809a1e331b
Sam Thurman <sam.bbmgmt@gmail.com>
2019-12-11 13:30:12 -0800

    got weather and location to render properly

--- server.js
+++ server.js
@@ -15,76 +15,56 @@ app.use(cors());
 const superagent = require("superagent");
 // Routes (for what? i dont know)
 
-function APIerror() {
-  return {
-    status: 500,
-    responseText: "Sorry, something went wrong"
-  };
-}
-
-//location
-//-----
-function Location(city, geoData) {
-  this.search_query = city;
-  this.formatted_query = geoData.results[0].formatted_address;
-  this.latitude = geoData.results[0].geometry.location.lat;
-  this.longitude = geoData.results[0].geometry.location.lng;
-}
-
 app.get("/location", (request, response) => {
   try {
     let city = request.query.data;
-    let locationObject = searchLatToLong(city);
-    console.log(locationObject);
-    response.send(locationObject);
+    let locationObj = searchLatToLong(city, response);
   } catch (error) {
     console.error(error);
     response.status(500).send("Sorry, something went wrong");
   }
 });
+// app.get("/location", searchLatToLong);
+app.get("/weather", searchWeather);
 
-function searchLatToLong(city) {
-  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`;
+//-----location
 
+function Location(request, geoData) {
+  this.search_query = request;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+function searchLatToLong(request, response) {
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request}&key=${process.env.GEOCODE_API_KEY}`;
   superagent.get(url).then(results => {
-    // console.log(results.body);
-    const locationObj = new Location(city, results.body);
+    const locationObj = new Location(request, results.body);
     console.log(
       `${locationObj.formatted_query} has a latitude of: ${locationObj.latitude} and a longitude of: ${locationObj.longitude}`
     );
-
-    return locationObj;
+    response.send(locationObj);
   });
 }
 
 //--------weather
 
 function Weather(weatherData) {
-  this.time = weatherData.time;
+  this.time = new Date(weatherData.time * 1000).toDateString();
   this.forecast = weatherData.summary;
 }
 
-app.get("/weather", (request, response) => {
-  try {
-    let weather = request.query.data;
-    let weatherObj = searchWeather(weather);
-    // console.log(weatherObj);
-    response.send(weatherObj);
-  } catch (error) {
-    console.error(error);
-    response.status(500).send("Sorry, something went wrong");
-  }
-});
+function searchWeather(request, response) {
+  let url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  let weatherArray = [];
 
-function searchWeather() {
-  const weatherData = require("./data/darksky.json");
-  const weatherDataResults = weatherData.daily.data;
-  const weatherDataArray = [];
-
-  weatherDataResults.map(weather => {
-    weatherDataArray.push(new Weather(weather));
+  superagent.get(url).then(results => {
+    results.body.daily.data.map(day => {
+      weatherArray.push(new Weather(day));
+    });
+    console.log(weatherArray);
+    response.send(weatherArray);
   });
-  return weatherDataArray;
 }
 
 app.get("*", (request, response) => {
@@ -92,3 +72,5 @@ app.get("*", (request, response) => {
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
+
+// events- parse the json results before you work with it
